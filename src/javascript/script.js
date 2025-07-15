2bbb3921dbbbc42debb4fc03d4bfedfb53d8c28a
Ygor Portes <ygorsportes@gmail.com>
2025-07-15 16:22:44 -0300

    feat: adiciona novo projeto e animações na página

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -10,3 +10,15 @@ window.addEventListener('scroll', function () {
   if (this.window.pageYOffset > 0) return navbar.classList.add('active');
   return navbar.classList.remove('active');
 });
+
+ScrollReveal().reveal('.header__left', {
+  origin: 'left',
+  duration: 2000,
+  distance: '20%'
+});
+
+ScrollReveal().reveal('.header__right', {
+  origin: 'right',
+  duration: 2000,
+  distance: '20%'
+});
